9bc2a3daf6086285049b14a58133ec87301b7c43
Ahnder <skguss1@gmail.com>
2019-12-30 09:21:08 +0900

    hello world! express

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hello World!!! Express node.js
+이 저장소의 코드는 nodejs 연습용입니다.
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');  // 설치한 express module 을 불러와서 변수에 담는다
+
+var app = express();  // express를 실행하여 app object 를 초기화
+// '/' 에 get 요청을 받는 경우, 콜백함수를 실행한다
+app.get('/', (req, res) => {
+    res.send("Hello world!!! Express-version");
+    //console.log(req);
+    //console.log(res);
+});
+
+// 사용할 포트번호를 port 변수에 할당
+var port = 3000;
+// 1. port 변수를 이용, 3000번 포트에 node.js 서버를 할당
+// 2. 서버가 실행되면 콜백함수를 실행
+app.listen(port, () => {
+    console.log("server on!! http://localhost:" + port);
+});
