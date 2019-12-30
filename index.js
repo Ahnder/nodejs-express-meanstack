dff9ec23ff8864d669ea2d98f435ba1c97f4840a
Ahnder <skguss1@gmail.com>
2019-12-30 09:47:16 +0900

    static config, create index.html & master.css

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express');  // 설치한 express module 을 불러와서 변수에 담는다
 
 var app = express();  // express를 실행하여 app object 를 초기화
-// '/' 에 get 요청을 받는 경우, 콜백함수를 실행한다
-app.get('/', (req, res) => {
-    res.send("Hello world!!! Express-version");
-    //console.log(req);
-    //console.log(res);
-});
+
+app.use(express.static(__dirname + '/public'));
 
 // 사용할 포트번호를 port 변수에 할당
 var port = 3000;
